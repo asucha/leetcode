0dbf87c8787702e07034d4d9382803ee5d11b4a8
asucha <anastasiasucha@pm.me>
2026-08-06 23:53:23 +0200

    added todays daily challange

---
+++ daily_problems/2026-08-august/2026-08-06_3345_ez_.py
@@ -0,0 +1,20 @@
+
+# leetcode nickname asucha_473109
+
+# 2026-08-02 leetcode daily problem --- easy --- 3345. Smallest Divisible Digit Product I --- solved
+
+"""
+You are given two integers n and t.
+Return the smallest number greater than or equal to n such that the product of its digits is divisible by t.
+"""
+
+# just took advantage of casting and recasting int into string and aggregating over each character
+
+from math import prod
+
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        while True:
+            if prod( int(d) for d in str(n) ) % t == 0:
+                return n
+            n += 1
