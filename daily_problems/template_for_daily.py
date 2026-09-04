7a0420724b03c0a5dffc9ef63a03a18eff653ff2
asucha <anastasiasucha@pm.me>
2026-09-04 23:25:17 +0200

    added template for problems solutions

---
+++ daily_problems/template_for_daily.py
@@ -0,0 +1,46 @@
+
+# leetcode nickname asucha_473109
+
+# 2026-<DATE> leetcode daily problem --- <solved/todo?>
+#   <NUMBER. NAME> --- <ez/mid/hard>
+
+# Topics:
+#
+
+"""
+
+PROBLEM DESCRIPTION
+
+
+
+PROBLEM CONSTRAINS
+
+
+
+PROBLEM EXAMPLES
+
+
+
+"""
+
+from typing import List
+
+class Solution:
+    def foo(self,param1: any) -> any:
+        pass
+
+
+# Simple Unit tests
+
+import unittest
+
+class SolutionTests(unittest.TestCase):
+
+    def test_change_name_haha(self):
+
+        testParam = []
+        self.assertTrue(Solution.foo(testParam))
+
+testResult = SolutionTests("test_change_name_haha").run()
+print(testResult.wasSuccessful())
+
